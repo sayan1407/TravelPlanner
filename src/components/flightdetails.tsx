f59ca0795e3bc2dac6d97c92b1f83b4c57e95176
Sayan Saha <sayan199783@gmail.com>
2026-09-08 23:50:51 +0530

    Change in flight details component to use city name if airport code is blank

--- src/components/flightdetails.tsx
+++ src/components/flightdetails.tsx
@@ -14,7 +14,8 @@ const FlightDetails: React.FC<FlightDetailsProps> = ({
         year: 'numeric',
       })
     : 'Select a date';
-
+  const fromAirport = from_location_airport_code === '' || from_location_airport_code.length === 0 ? from : from_location_airport_code;
+  const toAirport = to_location_airport_code === '' || to_location_airport_code.length === 0 || to_location_airport_code === undefined ? to : to_location_airport_code;
   return (
     <section className="flight-details-panel">
       <div className="flight-route">
@@ -39,7 +40,7 @@ const FlightDetails: React.FC<FlightDetailsProps> = ({
           <strong>{formattedDate}</strong>
         </div>
 
-        <button type="button" className="flight-book-button" onClick={() => window.open(`https://www.google.com/travel/flights?q=One%20way%20flights%20from%20${from_location_airport_code}%20to%20${to_location_airport_code}%20on%20${travelDate}`, '_blank')}>
+        <button type="button" className="flight-book-button" onClick={() => window.open(`https://www.google.com/travel/flights?q=One%20way%20flights%20from%20${fromAirport}%20to%20${toAirport}%20on%20${travelDate}`, '_blank')}>
           Book flight
         </button>
       </div>
